522cd1118c936b6632d76dd958124c20c0b7b726
Matt Monroe <matt.monroe@psyonix.com>
2024-12-03 09:38:20 -0800

    Solution for Day 1 Part 1

--- 01/main.go
+++ 01/main.go
@@ -3,8 +3,61 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
 	fmt.Fprintln(os.Stdout, "Hello World")
+}
+
+func ReadInput(filename string) ([]int, []int) {
+	content, err := os.ReadFile(filename)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to Read file: %s\n", filename)
+		return []int{}, []int{}
+	}
+
+	left := []int{}
+	right := []int{}
+	input := string(content)
+	for _, line := range strings.Split(input, "\n") {
+		if line == "" {
+			continue
+		}
+		splits := strings.Fields(strings.Trim(line, "\n"))
+		if len(splits) != 2 {
+			fmt.Fprintf(os.Stderr, "Improperly formatted input: '%s'\n", strings.Trim(line, "\n"))
+			continue
+		}
+		leftNum, err := strconv.Atoi(splits[0])
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Unable to parse leftNum for line: %s\n", splits[0])
+		}
+		left = append(left, leftNum)
+		rightNum, err := strconv.Atoi(splits[1])
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Unable to parse rightNum for line: %s\n", splits[1])
+		}
+		right = append(right, rightNum)
+	}
+
+	return left, right
+}
+
+func GetDiff(left []int, right []int) int {
+	diff := 0
+	for i, leftNum := range left {
+		rightNum := right[i]
+		diff += intAbs(leftNum - rightNum)
+	}
+
+	return diff
+}
+
+func intAbs(value int) int {
+	if value < 0 {
+		return -value
+	}
+	return value
 }
--- 01/main_test.go
+++ 01/main_test.go
@@ -1,7 +1,46 @@
 package main
 
-import "testing"
+import (
+	"slices"
+	"testing"
 
-func TestMain(t *testing.T) {
-	t.Logf("Ran Test")
+	"github.com/go-playground/assert/v2"
+)
+
+func TestSample1(t *testing.T) {
+	t.Logf("\n==== Testing Sample input for Problem 1 ====\n")
+
+	left, right := ReadInput("test.txt")
+	if len(left) == 0 {
+		t.Fatalf("Didn't get any left input\n")
+	}
+	if len(right) == 0 {
+		t.Fatalf("Didn't get any right input\n")
+	}
+	if len(left) != len(right) {
+		t.Fatalf("Left and Right have different number of items: %d - %d\n", len(left), len(right))
+	}
+	left = slices.Sorted[int](slices.Values(left))
+	right = slices.Sorted[int](slices.Values(right))
+	diff := GetDiff(left, right)
+	assert.Equal(t, diff, 11)
+}
+
+func TestFull1(t *testing.T) {
+	t.Logf("\n==== Testing Sample input for Problem 1 ====\n")
+
+	left, right := ReadInput("input.txt")
+	if len(left) == 0 {
+		t.Fatalf("Didn't get any left input\n")
+	}
+	if len(right) == 0 {
+		t.Fatalf("Didn't get any right input\n")
+	}
+	if len(left) != len(right) {
+		t.Fatalf("Left and Right have different number of items: %d - %d\n", len(left), len(right))
+	}
+	left = slices.Sorted[int](slices.Values(left))
+	right = slices.Sorted[int](slices.Values(right))
+	diff := GetDiff(left, right)
+	assert.Equal(t, diff, 2367773)
 }
