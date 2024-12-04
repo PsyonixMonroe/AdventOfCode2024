e8e944584561452b35ac21a002f8ea0fdc54a23f
Matt Monroe <matt.monroe@psyonix.com>
2024-12-04 09:11:21 -0800

    Day 2 Part 1 Solution

--- 02/main.go
+++ 02/main.go
@@ -3,8 +3,108 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
-func main() {
-	fmt.Fprintln(os.Stdout, "Hello World")
+type Report struct {
+	num    int
+	levels []int
+}
+
+func ParseInput(filename string) []Report {
+	content, err := os.ReadFile(filename)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to parse input file %v\n", err)
+		return []Report{}
+	}
+
+	reports := []Report{}
+	for i, line := range strings.Split(strings.Trim(string(content), "\n"), "\n") {
+		if line == "" {
+			continue
+		}
+		valuesStr := strings.Fields(strings.Trim(line, "\n"))
+		levels := []int{}
+		for _, v := range valuesStr {
+			level, err := strconv.Atoi(v)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "[%d] Unable to parse level, bad format: %s\n", i, line)
+				continue
+			}
+
+			levels = append(levels, level)
+		}
+
+		reports = append(reports, Report{num: i, levels: levels})
+	}
+
+	return reports
+}
+
+func IsAsc(report Report) bool {
+	for i, level := range report.levels {
+		if i == 0 {
+			continue
+		}
+		prevLevel := report.levels[i-1]
+		if prevLevel == level {
+			continue
+		}
+		return prevLevel < level
+	}
+
+	fmt.Fprintf(os.Stderr, "Unable to determine asc\n")
+	return true
+}
+
+func CountSafe(reports []Report) int {
+	safeCount := 0
+	for _, report := range reports {
+		isAsc := IsAsc(report)
+		var isSafe bool
+		if isAsc {
+			isSafe = IsSafeAsc(report)
+		} else {
+			isSafe = IsSafeDesc(report)
+		}
+
+		if isSafe {
+			safeCount++
+		}
+	}
+
+	return safeCount
+}
+
+func IsSafeAsc(report Report) bool {
+	for i, level := range report.levels {
+		if i == 0 {
+			continue
+		}
+		prevLevel := report.levels[i-1]
+		diff := level - prevLevel
+		// asc so diff should be positive between 1 and 3
+		if diff < 1 || diff > 3 {
+			return false
+		}
+	}
+
+	return true
+}
+
+func IsSafeDesc(report Report) bool {
+	for i, level := range report.levels {
+		if i == 0 {
+			continue
+		}
+		prevLevel := report.levels[i-1]
+		diff := level - prevLevel
+		// desc so diff should be negative between -1 and -3
+		if diff < -3 || diff > -1 {
+			return false
+		}
+	}
+
+	return true
 }
--- 02/main_test.go
+++ 02/main_test.go
@@ -1,7 +1,75 @@
 package main
 
-import "testing"
+import (
+	"testing"
 
-func TestMain(t *testing.T) {
-	t.Logf("Ran Test")
+	"github.com/go-playground/assert/v2"
+)
+
+func TestIsSafeAscSimple(t *testing.T) {
+	report := Report{num: 1, levels: []int{1, 3, 5, 7, 9}}
+	test := IsSafeAsc(report)
+	assert.Equal(t, test, true)
+}
+
+func TestIsSafeAscFailDesc(t *testing.T) {
+	report := Report{num: 1, levels: []int{9, 7, 5, 3, 1}}
+	test := IsSafeAsc(report)
+	assert.Equal(t, test, false)
+}
+
+func TestIsSafeAscFailRange(t *testing.T) {
+	report := Report{num: 1, levels: []int{1, 3, 5, 7, 12}}
+	test := IsSafeAsc(report)
+	assert.Equal(t, test, false)
+}
+
+func TestIsSafeAscFailRand(t *testing.T) {
+	report := Report{num: 1, levels: []int{1, 3, 15, 7, 9}}
+	test := IsSafeAsc(report)
+	assert.Equal(t, test, false)
+}
+
+func TestIsSafeDescSimple(t *testing.T) {
+	report := Report{num: 1, levels: []int{9, 7, 5, 3, 1}}
+	test := IsSafeDesc(report)
+	assert.Equal(t, test, true)
+}
+
+func TestIsSafeDescFailAsc(t *testing.T) {
+	report := Report{num: 1, levels: []int{1, 3, 5, 7, 9}}
+	test := IsSafeDesc(report)
+	assert.Equal(t, test, false)
+}
+
+func TestIsSafeDescFailRange(t *testing.T) {
+	report := Report{num: 1, levels: []int{15, 7, 5, 3, 1}}
+	test := IsSafeDesc(report)
+	assert.Equal(t, test, false)
+}
+
+func TestIsSafeDescFailRand(t *testing.T) {
+	report := Report{num: 1, levels: []int{8, 7, 15, 3, 1}}
+	test := IsSafeDesc(report)
+	assert.Equal(t, test, false)
+}
+
+func TestPart1Sample(t *testing.T) {
+	reports := ParseInput("test.txt")
+	if len(reports) == 0 {
+		t.Fatal("Failed to read in input file")
+	}
+
+	count := CountSafe(reports)
+	assert.Equal(t, count, 2)
+}
+
+func TestPart1Full(t *testing.T) {
+	reports := ParseInput("input.txt")
+	if len(reports) == 0 {
+		t.Fatal("Failed to read in input file")
+	}
+
+	count := CountSafe(reports)
+	assert.Equal(t, count, 257)
 }
