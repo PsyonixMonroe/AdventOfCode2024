4fb4dd2d9444d87c9f422766551c550ff4f87570
Matt Monroe <matt.a.monroe@gmail.com>
2024-12-09 21:06:22 -0700

    Day 8 Part 1 Solution

--- 08/main.go
+++ 08/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
 func ReadInput(filename string) string {
@@ -13,4 +14,127 @@ func ReadInput(filename string) string {
 	}
 
 	return string(content)
+}
+
+type Location struct {
+	station rune
+	points  []rune
+}
+
+type Map struct {
+	locs    []Location
+	rows    int
+	columns int
+}
+
+func (m Map) loc(row int, column int) int {
+	if row < 0 {
+		fmt.Fprintf(os.Stderr, "Unable to find loc, row is negative: %d\n", row)
+		return -1
+	}
+	if column < 0 {
+		fmt.Fprintf(os.Stderr, "Unable to find loc, column is negative: %d\n", column)
+		return -1
+	}
+	if column >= m.columns {
+		fmt.Fprintf(os.Stderr, "Unable to find loc, column is OOB: %d\n", column)
+		return -1
+	}
+	if row >= m.rows {
+		fmt.Fprintf(os.Stderr, "Unable to find loc, row is OOB: %d\n", row)
+		return -1
+	}
+	return row*m.columns + column
+}
+
+func ParseInput(content string) Map {
+	locs := []Location{}
+	rows := strings.Split(content, "\n")
+	numRows := len(rows)
+	if numRows == 0 {
+		fmt.Fprintf(os.Stderr, "No Rows Found\n")
+		return Map{}
+	}
+	numColumns := len(rows[0])
+
+	for _, val := range rows {
+		row := strings.Trim(val, "\n")
+		if row == "" {
+			numRows--
+			continue
+		}
+		for _, char := range row {
+			locs = append(locs, Location{station: char, points: []rune{}})
+		}
+	}
+
+	return Map{locs: locs, rows: numRows, columns: numColumns}
+}
+
+func MarkPoints(m *Map) {
+	for i := range m.rows {
+		for j := range m.columns {
+			loc := m.locs[m.loc(i, j)]
+			if loc.station == '.' {
+				continue
+			}
+			MarkStationPoints(m, loc.station, i, j)
+		}
+	}
+}
+
+func MarkStationPoints(m *Map, originStation rune, originX int, originY int) {
+	for i := range m.rows {
+		for j := range m.columns {
+			loc := m.locs[m.loc(i, j)]
+			if loc.station == originStation {
+				if i == originX && j == originY {
+					continue
+				}
+				// compute and mark points
+				var x1, y1, x2, y2 int
+				if originX < i {
+					x1, y1, x2, y2 = IntersectionsFromStation(originX, originY, i, j)
+				} else {
+					x1, y1, x2, y2 = IntersectionsFromStation(i, j, originX, originY)
+				}
+
+				if x1 >= 0 && x1 < m.rows && y1 >= 0 && y1 < m.columns {
+					m.locs[m.loc(x1, y1)].points = append(m.locs[m.loc(x1, y1)].points, originStation)
+				}
+				if x2 >= 0 && x2 < m.rows && y2 >= 0 && y2 < m.columns {
+					m.locs[m.loc(x2, y2)].points = append(m.locs[m.loc(x2, y2)].points, originStation)
+				}
+			}
+		}
+	}
+}
+
+func GetRiseAndRun(upperX int, upperY int, lowerX int, lowerY int) (int, int) {
+	rise := lowerX - upperX
+	run := lowerY - upperY
+	return rise, run
+}
+
+func IntersectionsFromStation(upperX int, upperY int, lowerX int, lowerY int) (int, int, int, int) {
+	rise, run := GetRiseAndRun(upperX, upperY, lowerX, lowerY)
+	highX := upperX - rise
+	lowX := lowerX + rise
+	highY := upperY - run
+	lowY := lowerY + run
+	return highX, highY, lowX, lowY
+}
+
+func CountLocations(m Map) int {
+	sum := 0
+	for i := range m.rows {
+		for j := range m.columns {
+			loc := m.locs[m.loc(i, j)]
+			if len(loc.points) > 0 {
+				sum++
+			}
+		}
+	}
+
+	return sum
 }
--- 08/main_test.go
+++ 08/main_test.go
@@ -9,4 +9,33 @@ import (
 func TestSimplePart1(t *testing.T) {
 	content := ReadInput("test.txt")
 	assert.NotEqual(t, content, "")
+	m := ParseInput(content)
+	MarkPoints(&m)
+	sum := CountLocations(m)
+	assert.Equal(t, sum, 14)
+}
+
+func TestIntersectionFromStation(t *testing.T) {
+	x1, y1, x2, y2 := IntersectionsFromStation(2, 2, 3, 3)
+	assert.Equal(t, x1, 1)
+	assert.Equal(t, y1, 1)
+	assert.Equal(t, x2, 4)
+	assert.Equal(t, y2, 4)
+}
+
+func TestIntersectionFromStationRev(t *testing.T) {
+	x1, y1, x2, y2 := IntersectionsFromStation(2, 3, 3, 2)
+	assert.Equal(t, x1, 1)
+	assert.Equal(t, y1, 4)
+	assert.Equal(t, x2, 4)
+	assert.Equal(t, y2, 1)
+}
+
+func TestFullPart1(t *testing.T) {
+	content := ReadInput("input.txt")
+	assert.NotEqual(t, content, "")
+	m := ParseInput(content)
+	MarkPoints(&m)
+	sum := CountLocations(m)
+	assert.Equal(t, sum, 361)
 }
