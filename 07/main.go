25f0957ec63ac998e7451f9e60f6434d921b32dc
Matt Monroe <matt.a.monroe@gmail.com>
2024-12-09 17:09:38 -0700

    Day 07 Part 1 Solution

--- 07/main.go
+++ 07/main.go
@@ -3,7 +3,14 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
+
+type Equation struct {
+	solution int
+	args     []int
+}
 
 func ReadInput(filename string) string {
 	content, err := os.ReadFile(filename)
@@ -13,4 +20,74 @@ func ReadInput(filename string) string {
 	}
 
 	return string(content)
+}
+
+func ParseInput(content string) []Equation {
+	equations := []Equation{}
+	for _, line := range strings.Split(strings.Trim(content, "\n"), "\n") {
+		eq := strings.Trim(line, "\n")
+		splits := strings.Split(eq, ":")
+		if len(splits) != 2 {
+			fmt.Fprintf(os.Stderr, "Incorrect format for line: %s\n", eq)
+			continue
+		}
+		solution, err := strconv.Atoi(splits[0])
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Unable to parse solution: %v\n", err)
+			continue
+		}
+
+		args := []int{}
+		for _, val := range strings.Fields(splits[1]) {
+			arg, err := strconv.Atoi(val)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Unable to parse argument: %v\n", err)
+				continue
+			}
+			args = append(args, arg)
+		}
+
+		equations = append(equations, Equation{solution: solution, args: args})
+	}
+
+	return equations
+}
+
+func SumGoodEquations(equations []Equation) int {
+	sum := 0
+	for _, equation := range equations {
+		sum += ProcessEquation(equation.solution, equation.args[0], equation.args[1:])
+	}
+
+	return sum
+}
+
+func ProcessEquation(solution int, lhs int, args []int) int {
+	if len(args) == 0 {
+		if lhs == solution {
+			return solution
+		} else {
+			return 0
+		}
+	}
+
+	var mulSol int
+	var addSol int
+	if len(args) == 1 {
+		mulSol = lhs * args[0]
+		addSol = lhs + args[0]
+	} else {
+		mulSol = ProcessEquation(solution, lhs*args[0], args[1:])
+		addSol = ProcessEquation(solution, lhs+args[0], args[1:])
+	}
+
+	if mulSol == solution {
+		return mulSol
+	}
+
+	if addSol == solution {
+		return addSol
+	}
+
+	return 0
 }
--- 07/main_test.go
+++ 07/main_test.go
@@ -9,4 +9,36 @@ import (
 func TestSimplePart1(t *testing.T) {
 	content := ReadInput("test.txt")
 	assert.NotEqual(t, content, "")
+	eq := ParseInput(content)
+	sum := SumGoodEquations(eq)
+	assert.Equal(t, sum, 3749)
+}
+
+func TestSumGoodEquations(t *testing.T) {
+	equations := []Equation{}
+	eq := Equation{solution: 190, args: []int{19, 10}}
+	equations = append(equations, eq)
+	sum := SumGoodEquations(equations)
+	assert.Equal(t, sum, 190)
+}
+
+func TestSumGoodEquationsReverse(t *testing.T) {
+	equations := []Equation{}
+	eq := Equation{solution: 190, args: []int{10, 19}}
+	equations = append(equations, eq)
+	sum := SumGoodEquations(equations)
+	assert.Equal(t, sum, 190)
+}
+
+func TestSlicing(t *testing.T) {
+	value := ProcessEquation(190, 19, []int{10})
+	assert.Equal(t, 190, value)
+}
+
+func TestFullPart1(t *testing.T) {
+	content := ReadInput("input.txt")
+	assert.NotEqual(t, content, "")
+	eq := ParseInput(content)
+	sum := SumGoodEquations(eq)
+	assert.Equal(t, sum, 2654749936343)
 }
