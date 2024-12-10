54a04cf44d38a77dfff87df816eece6b40e3027f
Matt Monroe <matt.a.monroe@gmail.com>
2024-12-10 10:17:35 -0700

    Day 09 Part 1 Solution

--- 09/main.go
+++ 09/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func ReadInput(filename string) string {
@@ -13,4 +15,77 @@ func ReadInput(filename string) string {
 	}
 
 	return string(content)
+}
+
+func swap(disk *[]int, i int, j int) {
+	tmp := (*disk)[i]
+	(*disk)[i] = (*disk)[j]
+	(*disk)[j] = tmp
+}
+
+func ParseInput(content string) []int {
+	disk := make([]int, 0)
+	for pos, size := range strings.Trim(content, "\n") {
+		num, err := strconv.Atoi(string(size))
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Invalid disk size: %s\n", string(size))
+			continue
+		}
+
+		var item int
+		if pos%2 == 0 {
+			// file
+			item = pos / 2
+		} else {
+			// empty space
+			item = -1
+		}
+		for range num {
+			disk = append(disk, item)
+		}
+	}
+
+	return disk
+}
+
+func DeFragFiles(disk *[]int) {
+	freeSpaceLoc := GetNextFreeSpaceLoc(*disk, 0)
+	for i := range len(*disk) {
+		moveFileLoc := len(*disk) - 1 - i
+		if moveFileLoc <= freeSpaceLoc {
+			// done
+			break
+		}
+		if (*disk)[moveFileLoc] == -1 {
+			continue
+		}
+		swap(disk, moveFileLoc, freeSpaceLoc)
+		freeSpaceLoc = GetNextFreeSpaceLoc(*disk, freeSpaceLoc+1)
+	}
+}
+
+func GetNextFreeSpaceLoc(disk []int, pos int) int {
+	maxLen := len(disk)
+	for i := range maxLen {
+		newPos := pos + i
+		if newPos >= maxLen {
+			return maxLen
+		}
+		if disk[newPos] == -1 {
+			// found free space
+			return newPos
+		}
+	}
+	return maxLen
+}
+
+func Checksum(disk []int) int {
+	sum := 0
+	for i, fileID := range disk {
+		if fileID < 0 {
+			continue
+		}
+		sum += i * fileID
+	}
+	return sum
 }
--- 09/main_test.go
+++ 09/main_test.go
@@ -9,4 +9,35 @@ import (
 func TestSimplePart1(t *testing.T) {
 	content := ReadInput("test.txt")
 	assert.NotEqual(t, content, "")
+
+	disk := ParseInput(content)
+	DeFragFiles(&disk)
+
+	checkSum := Checksum(disk)
+	assert.Equal(t, checkSum, 1928)
 }
+
+func TestParseInput(t *testing.T) {
+	disk := ParseInput("12345")
+	expectedDisk := []int{0, -1, -1, 1, 1, 1, -1, -1, -1, -1, 2, 2, 2, 2, 2}
+	assertEqual(t, disk, expectedDisk)
+}
+
+func assertEqual(t *testing.T, actual []int, expected []int) {
+	assert.Equal(t, len(actual), len(expected))
+	for i, val := range actual {
+		assert.Equal(t, val, expected[i])
+	}
+}
+
+func TestFullPart1(t *testing.T) {
+	content := ReadInput("input.txt")
+	assert.NotEqual(t, content, "")
+
+	disk := ParseInput(content)
+	DeFragFiles(&disk)
+
+	checkSum := Checksum(disk)
+	assert.Equal(t, checkSum, 6334655979668)
+}
+
