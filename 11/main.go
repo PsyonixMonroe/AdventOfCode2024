c19cf70507a80b88d7d119a06c1ab7c94f166582
Matt Monroe <matt.a.monroe@gmail.com>
2024-12-11 13:48:40 -0700

    Day 11 Part 1 Solution

--- 11/main.go
+++ 11/main.go
@@ -3,14 +3,50 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 )
 
-func ReadInput(filename string) string {
-	content, err := os.ReadFile(filename)
+func RunStonesSim(stones []int, iterations int) []int {
+	for range iterations {
+		newStones := []int{}
+		for i, stone := range stones {
+			if stone == 0 {
+				newStones = append(newStones, 1)
+				continue
+			}
+			if evenDigits(stone) {
+				// split stone
+				num1, num2 := splitDigits(stone)
+				newStones = append(newStones, num1)
+				newStones = append(newStones, num2)
+				continue
+			}
+			newStones = append(newStones, stone*2024)
+			stones[i] *= 2024
+		}
+		stones = newStones
+	}
+	return stones
+}
+
+func evenDigits(num int) bool {
+	numStr := strconv.Itoa(num)
+	return len(numStr)%2 == 0
+}
+
+func splitDigits(num int) (int, int) {
+	numStr := strconv.Itoa(num)
+	num1, err := strconv.Atoi(numStr[:len(numStr)/2])
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Unable to read file %s: %v\n", filename, err)
-		return ""
+		fmt.Fprintf(os.Stderr, "Unable to parse num1 from %s, len: %d\n", numStr, len(numStr)/2)
+		return -1, -1
 	}
 
-	return string(content)
+	num2, err := strconv.Atoi(numStr[len(numStr)/2:])
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to parse num2 from %s, len: %d\n", numStr, len(numStr)/2)
+		return -1, -1
+	}
+
+	return num1, num2
 }
--- 11/main_test.go
+++ 11/main_test.go
@@ -3,10 +3,55 @@ package main
 import (
 	"testing"
 
+	"github.com/PsyonixMonroe/AOCLib/lib"
 	"github.com/go-playground/assert/v2"
 )
 
 func TestSimplePart1(t *testing.T) {
-	content := ReadInput("test.txt")
+	content := lib.ReadInput("test.txt")
 	assert.NotEqual(t, content, "")
+
+	stones := lib.ParseIntLineFields(content)
+	stones = RunStonesSim(stones, 1)
+	assertEqual(t, stones, []int{1, 2024, 1, 0, 9, 9, 2021976})
+}
+
+func TestSimple2Part1(t *testing.T) {
+	content := lib.ReadInput("test2.txt")
+	assert.NotEqual(t, content, "")
+
+	stones := lib.ParseIntLineFields(content)
+	stones = RunStonesSim(stones, 6)
+	assertEqual(t, stones, []int{2097446912, 14168, 4048, 2, 0, 2, 4, 40, 48, 2024, 40, 48, 80, 96, 2, 8, 6, 7, 6, 0, 3, 2})
+}
+
+func TestSimple2LargePart1(t *testing.T) {
+	content := lib.ReadInput("test2.txt")
+	assert.NotEqual(t, content, "")
+
+	stones := lib.ParseIntLineFields(content)
+	stones = RunStonesSim(stones, 25)
+	assert.Equal(t, len(stones), 55312)
+}
+
+func assertEqual(t *testing.T, actual []int, expected []int) {
+	assert.Equal(t, len(actual), len(expected))
+	for i, val := range actual {
+		assert.Equal(t, val, expected[i])
+	}
+}
+
+func TestSplitDigits(t *testing.T) {
+	num1, num2 := splitDigits(1234)
+	assert.Equal(t, num1, 12)
+	assert.Equal(t, num2, 34)
+}
+
+func TestFullPart1(t *testing.T) {
+	content := lib.ReadInput("input.txt")
+	assert.NotEqual(t, content, "")
+
+	stones := lib.ParseIntLineFields(content)
+	stones = RunStonesSim(stones, 25)
+	assert.Equal(t, len(stones), 193269)
 }
