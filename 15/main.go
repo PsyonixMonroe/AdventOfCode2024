d98b668f6a27d94179ee716f3fe513f976d234ea
Matt Monroe <matt.a.monroe@gmail.com>
2024-12-15 12:22:41 -0700

    Day 15 Part 2 Solution

--- 15/main.go
+++ 15/main.go
@@ -114,3 +114,181 @@ func ScoreShop(shop Shop) int {
 	}
 	return score
 }
+
+func GetWideShop(shop Shop) Shop {
+	newShopGrid := lib.GetGrid[rune](shop.shop.Rows(), shop.shop.Columns()*2)
+	for i := range shop.shop.Rows() {
+		for j := range shop.shop.Columns() {
+			current := *shop.shop.Get(i, j).Get()
+			if current == '#' {
+				newShopGrid.Put('#', i, j*2)
+				newShopGrid.Put('#', i, j*2+1)
+			}
+			if current == 'O' {
+				newShopGrid.Put('[', i, j*2)
+				newShopGrid.Put(']', i, j*2+1)
+			}
+			if current == '@' {
+				newShopGrid.Put('@', i, j*2)
+				newShopGrid.Put('.', i, j*2+1)
+			}
+			if current == '.' {
+				newShopGrid.Put('.', i, j*2)
+				newShopGrid.Put('.', i, j*2+1)
+			}
+		}
+	}
+
+	return Shop{shop: newShopGrid, cursor: lib.Location{X: 0, Y: 0}}
+}
+
+func ProcessMovesWide(shop *Shop, moves []Move) {
+	for _, move := range moves {
+		if tryMove(*shop, move, shop.cursor) {
+			moveDirectionWide(shop, move, shop.cursor)
+		}
+	}
+}
+
+func tryMove(shop Shop, move Move, current lib.Location) bool {
+	if move.X == 0 {
+		return tryMoveHorizontal(shop, move, current)
+	}
+	return tryMoveVertical(shop, move, current)
+}
+
+func tryMoveHorizontal(shop Shop, move Move, current lib.Location) bool {
+	nextLoc := lib.Location{X: current.X, Y: current.Y + move.Y}
+	nextNextLoc := lib.Location{X: current.X, Y: current.Y + move.Y*2}
+	if shop.shop.IsOOBLocation(nextLoc) {
+		return false
+	}
+	nextItem := *shop.shop.GetLocation(nextLoc).Get()
+
+	if nextItem == '#' {
+		return false
+	}
+
+	if nextItem == '.' {
+		return true
+	}
+
+	if nextItem == '[' || nextItem == ']' {
+		return tryMoveHorizontal(shop, move, nextNextLoc)
+	}
+
+	fmt.Fprintf(os.Stderr, "[tryMoveHorizontal] Unkown item for Next Item: %s\n", string(nextItem))
+	return false
+}
+
+func tryMoveVertical(shop Shop, move Move, current lib.Location) bool {
+	adjLoc := lib.Location{X: current.X + move.X, Y: current.Y}
+
+	if shop.shop.IsOOBLocation(adjLoc) {
+		return false
+	}
+
+	adjItem := *shop.shop.GetLocation(adjLoc).Get()
+
+	if adjItem == '.' {
+		return true
+	}
+
+	if adjItem == '#' {
+		return false
+	}
+
+	if adjItem == '[' {
+		diagLoc := lib.Location{X: adjLoc.X, Y: adjLoc.Y + 1}
+		return tryMoveVertical(shop, move, adjLoc) && tryMoveVertical(shop, move, diagLoc)
+	}
+
+	if adjItem == ']' {
+		diagLoc := lib.Location{X: adjLoc.X, Y: adjLoc.Y - 1}
+		return tryMoveVertical(shop, move, adjLoc) && tryMoveVertical(shop, move, diagLoc)
+	}
+
+	fmt.Fprintf(os.Stderr, "[tryMoveVertical] Unknown item at next location: %s\n", string(adjItem))
+	return false
+}
+
+func moveDirectionWide(shop *Shop, move Move, current lib.Location) {
+	if move.X == 0 {
+		moveDirectionWideHorizontal(shop, move, current)
+	}
+	moveDirectionWideVertical(shop, move, current)
+}
+
+func moveDirectionWideHorizontal(shop *Shop, move Move, current lib.Location) {
+	nextLoc := lib.Location{X: current.X, Y: current.Y + move.Y}
+	nextNextLoc := lib.Location{X: current.X, Y: current.Y + move.Y*2}
+	nextItem := *shop.shop.GetLocation(nextLoc).Get()
+
+	if nextItem == '.' {
+		curItem := *shop.shop.GetLocation(current).Get()
+		shop.shop.PutLocation(curItem, nextLoc)
+		shop.shop.PutLocation('.', current)
+		if curItem == '@' {
+			shop.cursor = nextLoc
+		}
+		return
+	}
+
+	if nextItem == '[' || nextItem == ']' {
+		nextNextItem := *shop.shop.GetLocation(nextNextLoc).Get()
+		if nextNextItem != '.' {
+			moveDirectionWideHorizontal(shop, move, nextNextLoc)
+		}
+		moveDirectionWideHorizontal(shop, move, nextLoc)
+		moveDirectionWideHorizontal(shop, move, current)
+		return
+	}
+
+	fmt.Fprintf(os.Stderr, "[moveDirectionWideHorizontal] Unkown item for Next Item: %s\n", string(nextItem))
+}
+
+func moveDirectionWideVertical(shop *Shop, move Move, current lib.Location) {
+	adjLoc := lib.Location{X: current.X + move.X, Y: current.Y}
+	adjItem := *shop.shop.GetLocation(adjLoc).Get()
+
+	if adjItem == '.' {
+		curItem := *shop.shop.GetLocation(current).Get()
+		shop.shop.PutLocation(curItem, adjLoc)
+		shop.shop.PutLocation('.', current)
+		if curItem == '@' {
+			shop.cursor = adjLoc
+		}
+		return
+	}
+
+	if adjItem == '[' {
+		diagLoc := lib.Location{X: adjLoc.X, Y: adjLoc.Y + 1}
+		moveDirectionWideVertical(shop, move, adjLoc)
+		moveDirectionWideVertical(shop, move, diagLoc)
+		moveDirectionWideVertical(shop, move, current)
+		return
+	}
+
+	if adjItem == ']' {
+		diagLoc := lib.Location{X: adjLoc.X, Y: adjLoc.Y - 1}
+		moveDirectionWideVertical(shop, move, adjLoc)
+		moveDirectionWideVertical(shop, move, diagLoc)
+		moveDirectionWideVertical(shop, move, current)
+		return
+	}
+
+	fmt.Fprintf(os.Stderr, "[moveDirectionWideVertical] Unknown item at next location: %s\n", string(adjItem))
+}
+
+func ScoreShopWide(shop Shop) int {
+	score := 0
+	for i := range shop.shop.Rows() {
+		for j := range shop.shop.Columns() {
+			slot := *shop.shop.Get(i, j).Get()
+			if slot == '[' {
+				score += 100*i + j
+			}
+		}
+	}
+	return score
+}
--- 15/main_test.go
+++ 15/main_test.go
@@ -52,3 +52,46 @@ func TestFullPart1(t *testing.T) {
 
 	assert.Equal(t, sum, 1492518)
 }
+
+func TestSimplePart2(t *testing.T) {
+	content := lib.ReadInput("test.txt")
+	assert.NotEqual(t, content, "")
+
+	shop, moves := ParseShop(content)
+	shop = GetWideShop(shop)
+	shop.cursor = FindCursor(shop.shop)
+	assert.NotEqual(t, len(moves), 0)
+	ProcessMovesWide(&shop, moves)
+	sum := ScoreShopWide(shop)
+
+	assert.Equal(t, sum, 1751)
+}
+
+func TestSimple2Part2(t *testing.T) {
+	content := lib.ReadInput("test2.txt")
+	assert.NotEqual(t, content, "")
+
+	shop, moves := ParseShop(content)
+	shop = GetWideShop(shop)
+	shop.cursor = FindCursor(shop.shop)
+	assert.NotEqual(t, len(moves), 0)
+	ProcessMovesWide(&shop, moves)
+	sum := ScoreShopWide(shop)
+
+	assert.Equal(t, sum, 9021)
+}
+
+func TestFullPart2(t *testing.T) {
+	content := lib.ReadInput("input.txt")
+	assert.NotEqual(t, content, "")
+
+	shop, moves := ParseShop(content)
+	shop = GetWideShop(shop)
+	shop.cursor = FindCursor(shop.shop)
+	// shop.shop.Print(lib.PrintRune)
+	assert.NotEqual(t, len(moves), 0)
+	ProcessMovesWide(&shop, moves)
+	sum := ScoreShopWide(shop)
+
+	assert.Equal(t, sum, 1512860)
+}
