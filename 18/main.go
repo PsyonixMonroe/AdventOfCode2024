db69828f69a4d7d76303c1f097823903cbfeee0c
Matt Monroe <matt.a.monroe@gmail.com>
2024-12-18 13:26:49 -0700

    Day 18 Part 1 Solution

--- 18/main.go
+++ 18/main.go
@@ -3,14 +3,61 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
+
+	"github.com/PsyonixMonroe/AOCLib/lib"
 )
 
-func ReadInput(filename string) string {
-	content, err := os.ReadFile(filename)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Unable to read file %s: %v\n", filename, err)
-		return ""
+func ParseBytes(content string, rows int, columns int) (lib.Grid[rune], []lib.Location) {
+	grid := lib.GetGrid[rune](rows, columns)
+	locations := []lib.Location{}
+	for _, line := range strings.Split(strings.Trim(content, "\n"), "\n") {
+		coords := strings.Trim(line, "\n \t")
+		splits := strings.Split(coords, ",")
+		if len(splits) != 2 {
+			fmt.Fprintf(os.Stderr, "Unable to parse splits from coords: %s\n", coords)
+			continue
+		}
+		y, err := strconv.Atoi(splits[0])
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Unable to parse y coord: %s\n", splits[0])
+			continue
+		}
+
+		x, err := strconv.Atoi(splits[1])
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Unable to parse x coord: %s\n", splits[0])
+			continue
+		}
+
+		locations = append(locations, lib.Location{X: x, Y: y})
 	}
 
-	return string(content)
+	for i := range rows {
+		for j := range columns {
+			grid.Put('.', i, j)
+		}
+	}
+
+	return grid, locations
+}
+
+func FindPath(grid lib.Grid[rune], start lib.Location, end lib.Location, bytes []lib.Location, numBytes int) []lib.Location {
+	if len(bytes) <= numBytes {
+		fmt.Fprintf(os.Stderr, "Unable to fill in bytes, not enough bytes provided\n")
+		return []lib.Location{}
+	}
+	for i := range numBytes {
+		byte := bytes[i]
+		grid.Put('#', byte.X, byte.Y)
+	}
+
+	path := lib.FindPath(grid, start, end, isWall, lib.SimpleGridDistanceHVal, lib.SimpleNeighborGDiff)
+	path, _ = lib.RemoveItem(path, start)
+	return path
+}
+
+func isWall(g lib.Grid[rune], loc lib.Location) bool {
+	return g.IsOOBLocation(loc) || *g.GetLocation(loc).Get() == '#'
 }
--- 18/main_test.go
+++ 18/main_test.go
@@ -3,10 +3,40 @@ package main
 import (
 	"testing"
 
+	"github.com/PsyonixMonroe/AOCLib/lib"
 	"github.com/go-playground/assert/v2"
 )
 
 func TestSimplePart1(t *testing.T) {
-	content := ReadInput("test.txt")
+	// test parameters
+	gridRows := 7
+	gridColumns := 7
+	numBytes := 12
+
+	content := lib.ReadInput("test.txt")
 	assert.NotEqual(t, content, "")
+	grid, byteLocations := ParseBytes(content, gridRows, gridColumns)
+	assert.NotEqual(t, len(byteLocations), 0)
+
+	start := lib.Location{X: 0, Y: 0}
+	end := lib.Location{X: gridRows - 1, Y: gridColumns - 1}
+	path := FindPath(grid, start, end, byteLocations, numBytes)
+	assert.Equal(t, len(path), 22)
+}
+
+func TestFullPart1(t *testing.T) {
+	// test parameters
+	gridRows := 71
+	gridColumns := 71
+	numBytes := 1024
+
+	content := lib.ReadInput("input.txt")
+	assert.NotEqual(t, content, "")
+	grid, byteLocations := ParseBytes(content, gridRows, gridColumns)
+	assert.NotEqual(t, len(byteLocations), 0)
+
+	start := lib.Location{X: 0, Y: 0}
+	end := lib.Location{X: gridRows - 1, Y: gridColumns - 1}
+	path := FindPath(grid, start, end, byteLocations, numBytes)
+	assert.Equal(t, len(path), 322)
 }
