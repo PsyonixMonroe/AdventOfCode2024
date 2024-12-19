b8f63c3dd5b5d636424b3cd893597153647dc67a
Matt Monroe <matt.a.monroe@gmail.com>
2024-12-19 09:34:50 -0700

    Day 19 Part 1 Solution
    - also includes a trie solution, but it won't work on the full input
      (dies from OOM)

--- 19/main.go
+++ 19/main.go
@@ -3,14 +3,158 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
+
+	"github.com/PsyonixMonroe/AOCLib/lib"
 )
 
-func ReadInput(filename string) string {
-	content, err := os.ReadFile(filename)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Unable to read file %s: %v\n", filename, err)
-		return ""
+func ParseInput(content string) ([]string, []string) {
+	lines := strings.Split(strings.Trim(content, " \n\t"), "\n")
+	if len(lines) < 3 {
+		fmt.Fprintf(os.Stderr, "Unable to parse input content: %s\n", content)
+		return []string{}, []string{}
 	}
 
-	return string(content)
+	words := []string{}
+	patterns := []string{}
+
+	for _, pattern := range strings.Split(strings.Trim(lines[0], "\n \t"), ",") {
+		patterns = append(patterns, strings.Trim(pattern, ", \n\t"))
+	}
+
+	// lines[0] => patterns
+	// lines[1] => blank
+	// lines[2] => first word
+	// lines[3] => second word...
+	for _, line := range lines[2:] {
+		word := strings.Trim(line, "\n \t")
+		if word == "" {
+			fmt.Fprintf(os.Stderr, "Not adding empty string to words")
+			continue
+		}
+		words = append(words, word)
+	}
+
+	return patterns, words
+}
+
+func CountUniqueWordsIter(patterns []string, words []string) int {
+	sum := 0
+	for _, word := range words {
+		if FindWordFromPrefix(patterns, "", word) {
+			sum++
+		}
+	}
+
+	return sum
+}
+
+func FindWordFromPrefix(patterns []string, prefix string, fullWord string) bool {
+	if prefix == fullWord {
+		return true
+	}
+
+	for _, pattern := range patterns {
+		newPrefix := prefix + pattern
+		if strings.HasPrefix(fullWord, newPrefix) {
+			found := FindWordFromPrefix(patterns, newPrefix, fullWord)
+			if found {
+				// found a match, that is all we need
+				return true
+			}
+		}
+	}
+
+	return false
+}
+
+type TrieNode struct {
+	children map[string]TrieNode
+	pattern  string
+	prefix   string
+}
+
+func (t TrieNode) IsWordMatch() bool {
+	return len(t.children) == 0 && t.pattern == ""
+}
+
+func (t TrieNode) IsRoot() bool {
+	return len(t.children) > 0 && t.pattern == ""
+}
+
+func (t TrieNode) GetMatchingChildren(current string, fullWord string) []TrieNode {
+	matchNodes := []TrieNode{}
+	for child, node := range t.children {
+		childString := current + child
+		if strings.HasPrefix(fullWord, childString) {
+			matchNodes = append(matchNodes, node)
+		}
+	}
+
+	return matchNodes
+}
+
+func BuildTrie(patterns []string, words []string) TrieNode {
+	root := TrieNode{children: make(map[string]TrieNode), pattern: "", prefix: ""}
+	for _, word := range words {
+		GetTrieChildren(patterns, "", word, &root)
+	}
+
+	return root
+}
+
+func GetTrieChildren(patterns []string, prefix string, fullWord string, current *TrieNode) {
+	if prefix == fullWord {
+		// found a leaf
+		current.children[""] = TrieNode{children: make(map[string]TrieNode), pattern: "", prefix: prefix}
+		return
+	}
+
+	for _, pattern := range patterns {
+		childPrefix := prefix + pattern
+		if strings.HasPrefix(fullWord, childPrefix) {
+			childNode, found := current.children[pattern]
+			if !found {
+				childNode = TrieNode{children: make(map[string]TrieNode), pattern: pattern, prefix: childPrefix}
+			}
+			GetTrieChildren(patterns, childPrefix, fullWord, &childNode)
+			current.children[pattern] = childNode
+		}
+	}
+}
+
+func CountAllWordsTrie(root TrieNode) int {
+	work := lib.NewQueue[TrieNode]()
+	work.Enqueue(root)
+	count := 0
+	for !work.IsEmpty() {
+		current := work.Dequeue().Get()
+		if current.IsWordMatch() {
+			count++
+			continue
+		}
+		for _, child := range current.children {
+			work.Enqueue(child)
+		}
+	}
+
+	return count
+}
+
+func CountUniqueWordsTrie(root TrieNode) int {
+	work := lib.NewQueue[TrieNode]()
+	work.Enqueue(root)
+	words := []string{}
+	for !work.IsEmpty() {
+		current := work.Dequeue().Get()
+		if current.IsWordMatch() {
+			words = lib.AppendDistinct(words, current.prefix)
+			continue
+		}
+		for _, child := range current.children {
+			work.Enqueue(child)
+		}
+	}
+
+	return len(words)
 }
--- 19/main_test.go
+++ 19/main_test.go
@@ -3,10 +3,53 @@ package main
 import (
 	"testing"
 
+	"github.com/PsyonixMonroe/AOCLib/lib"
 	"github.com/go-playground/assert/v2"
 )
 
-func TestSimplePart1(t *testing.T) {
-	content := ReadInput("test.txt")
+func TestSimpleTriePart1(t *testing.T) {
+	content := lib.ReadInput("test.txt")
 	assert.NotEqual(t, content, "")
+	patterns, words := ParseInput(content)
+	assert.NotEqual(t, len(patterns), 0)
+	assert.NotEqual(t, len(words), 0)
+
+	root := BuildTrie(patterns, words)
+	sum := CountUniqueWordsTrie(root)
+	assert.Equal(t, sum, 6)
 }
+
+func TestSimpleIterPart1(t *testing.T) {
+	content := lib.ReadInput("test.txt")
+	assert.NotEqual(t, content, "")
+	patterns, words := ParseInput(content)
+	assert.NotEqual(t, len(patterns), 0)
+	assert.NotEqual(t, len(words), 0)
+
+	sum := CountUniqueWordsIter(patterns, words)
+	assert.Equal(t, sum, 6)
+}
+
+func TestFullIterPart1(t *testing.T) {
+	content := lib.ReadInput("input.txt")
+	assert.NotEqual(t, content, "")
+	patterns, words := ParseInput(content)
+	assert.NotEqual(t, len(patterns), 0)
+	assert.NotEqual(t, len(words), 0)
+
+	sum := CountUniqueWordsIter(patterns, words)
+	assert.Equal(t, sum, 240)
+}
+
+// func TestFullPart1(t *testing.T) {
+// 	content := lib.ReadInput("input.txt")
+// 	assert.NotEqual(t, content, "")
+// 	patterns, words := ParseInput(content)
+// 	assert.NotEqual(t, len(patterns), 0)
+// 	assert.NotEqual(t, len(words), 0)
+//
+// 	root := BuildTrie(patterns, words)
+// 	fmt.Fprintf(os.Stderr, "Finished Building Trie\n")
+// 	sum := CountUniqueWordsTrie(root)
+// 	assert.Equal(t, sum, 6)
+// }
