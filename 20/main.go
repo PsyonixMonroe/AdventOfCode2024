2771b5c4f90d86d7b3e09bb71d5306f3bee0c52a
Matt Monroe <matt.a.monroe@gmail.com>
2024-12-20 20:28:31 -0700

    Day 20 Part 2 Solution

--- 20/main.go
+++ 20/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"context"
+	"sync"
+
 	"github.com/PsyonixMonroe/AOCLib/lib"
 )
 
@@ -90,4 +93,146 @@ func openNeighbors(g lib.Grid[rune], i int, j int, isWall func(g lib.Grid[rune],
 
 func isWall(g lib.Grid[rune], loc lib.Location) bool {
 	return *g.GetLocation(loc).Get() == '#'
+}
+
+type CheatProcReq struct {
+	start lib.Location
+	dist  int
+}
+
+func CountCheats(track RaceTrack, minSavings int) int {
+	basePath := lib.FindPath(track.track, track.start, track.end, isWall, lib.SimpleGridDistanceHVal, lib.SimpleNeighborGDiff)
+	base := len(basePath) - 1
+	sum := 0
+	startPositions := make(chan CheatProcReq, 100)
+	endPositions := make(chan lib.Location, 100)
+	procCtx, procCancel := context.WithCancel(context.Background())
+	procwg := sync.WaitGroup{}
+	procwg.Add(12)
+	endPosCache := GetEmptyDistGrid(track.track)
+	for range 12 {
+		go func() {
+			defer procwg.Done()
+			done := false
+			for {
+				select {
+				case <-procCtx.Done():
+					done = true
+				case s := <-startPositions:
+					endPos := GetEndPositions(track, &endPosCache, s, minSavings, base)
+					for _, end := range endPos {
+						endPositions <- end
+					}
+				}
+				if done {
+					break
+				}
+			}
+		}()
+	}
+
+	endCtx, endCancel := context.WithCancel(context.Background())
+	endwg := sync.WaitGroup{}
+	endwg.Add(1)
+	go func() {
+		defer endwg.Done()
+		done := false
+		for {
+			select {
+			case <-endCtx.Done():
+				done = true
+			case <-endPositions:
+				sum++
+			}
+			if done {
+				break
+			}
+		}
+	}()
+
+	// generate Start positions
+	for i := range track.track.Rows() {
+		for j := range track.track.Columns() {
+			startCheatLoc := lib.Location{X: i, Y: j}
+			if !isWall(track.track, startCheatLoc) {
+				// we always start on a track at index -1
+				startPath := lib.FindPath(track.track, track.start, startCheatLoc, isWall, lib.SimpleGridDistanceHVal, lib.SimpleNeighborGDiff)
+				startPathLen := len(startPath) - 1
+				if startPathLen > base-minSavings {
+					// no improvement will get us where we need to go
+					continue
+				}
+				startPositions <- CheatProcReq{start: startCheatLoc, dist: startPathLen}
+			}
+		}
+	}
+
+	// Finish all the work
+	for len(startPositions) > 0 {
+	}
+	procCancel()
+	procwg.Wait()
+
+	for len(endPositions) > 0 {
+	}
+	endCancel()
+	endwg.Wait()
+
+	return sum
+}
+
+func GetEmptyDistGrid(grid lib.Grid[rune]) lib.Grid[int] {
+	distGrid := lib.GetGrid[int](grid.Rows(), grid.Columns())
+	for i := range distGrid.Rows() {
+		for j := range distGrid.Columns() {
+			distGrid.Put(-1, i, j)
+		}
+	}
+
+	return distGrid
+}
+
+func GetEndPositions(track RaceTrack, distGrid *lib.Grid[int], req CheatProcReq, minSavings int, base int) []lib.Location {
+	const cheatLength = 20
+	directions := []lib.Location{{X: -1, Y: 1}, {X: 1, Y: 1}, {X: -1, Y: -1}, {X: 1, Y: -1}}
+	endPositions := []lib.Location{}
+	for i := range cheatLength + 1 {
+		for j := range cheatLength + 1 {
+			if (i + j) > cheatLength {
+				continue
+			}
+			for _, dir := range directions {
+				testLoc := lib.Location{X: req.start.X + i*dir.X, Y: req.start.Y + j*dir.Y}
+				if !track.track.IsOOBLocation(testLoc) && !isWall(track.track, testLoc) && !LocInArray(testLoc, endPositions) {
+					distGridVal := *distGrid.GetLocation(testLoc).Get()
+					if distGridVal == -1 {
+						// compute and save dist
+						endPath := lib.FindPath(track.track, testLoc, track.end, isWall, lib.SimpleGridDistanceHVal, lib.SimpleNeighborGDiff)
+						distGridVal = len(endPath) - 1
+						distGrid.PutLocation(distGridVal, testLoc)
+					}
+
+					if req.dist+i+j+distGridVal <= (base - minSavings) {
+						// fmt.Fprintf(os.Stderr, "Found Cheat: %d saves %d - Start:%v End:%v\nBaseDist: %d, reqDist: %d, i:%d, j:%d, endPathDist: %d\n", 1+i+j, base-(req.dist+i+j+distGridVal), req.start, testLoc, base, req.dist, i, j, distGridVal)
+						endPositions = append(endPositions, testLoc)
+						// } else {
+						// if req.dist+i+j+distGridVal-2 <= (base - minSavings) {
+						// 	fmt.Fprintf(os.Stderr, "Near Miss: %d saves %d - Start:%v End:%v\nBaseDist: %d, reqDist: %d, i:%d, j:%d, endPathDist: %d\n", 1+i+j, base-(req.dist+i+j+distGridVal), req.start, testLoc, base, req.dist, i, j, distGridVal)
+						// }
+					}
+				}
+			}
+		}
+	}
+
+	return endPositions
+}
+
+func LocInArray(needle lib.Location, haystack []lib.Location) bool {
+	for _, straw := range haystack {
+		if straw.Equal(needle) {
+			return true
+		}
+	}
+	return false
 }
--- 20/main_test.go
+++ 20/main_test.go
@@ -25,3 +25,61 @@ func TestFullPart1(t *testing.T) {
 	sum := CountGoodPaths(track, content, 100)
 	assert.Equal(t, sum, 1375)
 }
+
+func TestSimplePart2(t *testing.T) {
+	content := lib.ReadInput("test.txt")
+	assert.NotEqual(t, content, "")
+	track := ParseInput(content)
+	var sum int
+
+	sum = CountCheats(track, 76)
+	assert.Equal(t, sum, 3)
+
+	sum = CountCheats(track, 74)
+	assert.Equal(t, sum, 7)
+
+	sum = CountCheats(track, 72)
+	assert.Equal(t, sum, 29)
+
+	sum = CountCheats(track, 70)
+	assert.Equal(t, sum, 41)
+
+	sum = CountCheats(track, 68)
+	assert.Equal(t, sum, 55)
+
+	sum = CountCheats(track, 66)
+	assert.Equal(t, sum, 67)
+
+	sum = CountCheats(track, 64)
+	assert.Equal(t, sum, 86)
+
+	sum = CountCheats(track, 62)
+	assert.Equal(t, sum, 106)
+
+	sum = CountCheats(track, 60)
+	assert.Equal(t, sum, 129)
+
+	sum = CountCheats(track, 58)
+	assert.Equal(t, sum, 154)
+
+	sum = CountCheats(track, 56)
+	assert.Equal(t, sum, 193)
+
+	sum = CountCheats(track, 54)
+	assert.Equal(t, sum, 222)
+
+	sum = CountCheats(track, 52)
+	assert.Equal(t, sum, 253)
+
+	sum = CountCheats(track, 50)
+	assert.Equal(t, sum, 285)
+}
+
+func TestFullPart2(t *testing.T) {
+	content := lib.ReadInput("input.txt")
+	assert.NotEqual(t, content, "")
+	track := ParseInput(content)
+
+	sum := CountCheats(track, 100)
+	assert.Equal(t, sum, 983054)
+}
