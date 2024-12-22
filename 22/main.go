86c9c2e133907890c6e4c46ebdd4f7c7135ae5ce
Matt Monroe <matt.a.monroe@gmail.com>
2024-12-22 10:38:55 -0700

    Day 22 Part 1 Solution

--- 22/main.go
+++ 22/main.go
@@ -3,14 +3,47 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
-func ReadInput(filename string) string {
-	content, err := os.ReadFile(filename)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Unable to read file %s: %v\n", filename, err)
-		return ""
+func GetSeeds(content string) []int {
+	lines := strings.Split(strings.Trim(content, "\n \t"), "\n")
+	values := []int{}
+	for _, line := range lines {
+		strVal := strings.Trim(line, "\n \t")
+		val, err := strconv.Atoi(strVal)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Unable to parse input line: %s\n", strVal)
+		}
+
+		values = append(values, val)
+	}
+	return values
+}
+
+func GetNumForSeedIterations(seed int, iterations int) int {
+	secret := seed
+	for range iterations {
+		next := secret * 64
+		secret = Mix(next, secret)
+		secret = Prune(secret)
+		next = secret / 32
+		secret = Mix(next, secret)
+		secret = Prune(secret)
+		next = secret * 2048
+		secret = Mix(next, secret)
+		secret = Prune(secret)
 	}
 
-	return string(content)
+	return secret
+}
+
+func Mix(val int, current int) int {
+	return val ^ current
+}
+
+func Prune(val int) int {
+	// note 16777216 is 2^23
+	return val % 16777216
 }
--- 22/main_test.go
+++ 22/main_test.go
@@ -3,10 +3,64 @@ package main
 import (
 	"testing"
 
+	"github.com/PsyonixMonroe/AOCLib/lib"
 	"github.com/go-playground/assert/v2"
 )
 
 func TestSimplePart1(t *testing.T) {
-	content := ReadInput("test.txt")
+	content := lib.ReadInput("test.txt")
 	assert.NotEqual(t, content, "")
+	seeds := GetSeeds(content)
+	sum := 0
+	for _, seed := range seeds {
+		sum += GetNumForSeedIterations(seed, 2000)
+	}
+
+	assert.Equal(t, sum, 37327623)
+}
+
+func TestFirstSeedIterations(t *testing.T) {
+	seeds := []int{123, 15887950, 16495136, 527345, 704524, 1553684, 12683156, 11100544, 12249484, 7753432, 5908254}
+	for i, seed := range seeds {
+		if i == len(seeds)-1 {
+			continue
+		}
+		result := GetNumForSeedIterations(seed, 1)
+		assert.Equal(t, result, seeds[i+1])
+	}
+}
+
+func TestEachSeedPart1(t *testing.T) {
+	seedResults := make(map[int]int)
+	seedResults[1] = 8685429
+	seedResults[10] = 4700978
+	seedResults[100] = 15273692
+	seedResults[2024] = 8667524
+
+	for seed, result := range seedResults {
+		calc := GetNumForSeedIterations(seed, 2000)
+		assert.Equal(t, calc, result)
+	}
+}
+
+func TestPrune(t *testing.T) {
+	val := Prune(100000000)
+	assert.Equal(t, val, 16113920)
+}
+
+func TestMix(t *testing.T) {
+	val := Mix(15, 42)
+	assert.Equal(t, val, 37)
+}
+
+func TestFullPart1(t *testing.T) {
+	content := lib.ReadInput("input.txt")
+	assert.NotEqual(t, content, "")
+	seeds := GetSeeds(content)
+	sum := 0
+	for _, seed := range seeds {
+		sum += GetNumForSeedIterations(seed, 2000)
+	}
+
+	assert.Equal(t, sum, 18317943467)
 }
