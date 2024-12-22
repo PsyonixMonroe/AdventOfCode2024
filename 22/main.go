8c3db3509dbc29297b506026d675f8c1c4d1db85
Matt Monroe <matt.a.monroe@gmail.com>
2024-12-22 11:45:29 -0700

    Day 22 Part 2 Solution

--- 22/main.go
+++ 22/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
 	"strconv"
 	"strings"
+	"sync"
 )
 
 func GetSeeds(content string) []int {
@@ -39,6 +41,25 @@ func GetNumForSeedIterations(seed int, iterations int) int {
 	return secret
 }
 
+func GetAllSeedIterations(seed int, iterations int) []int {
+	secret := seed
+	allSecrets := []int{secret}
+	for range iterations {
+		next := secret * 64
+		secret = Mix(next, secret)
+		secret = Prune(secret)
+		next = secret / 32
+		secret = Mix(next, secret)
+		secret = Prune(secret)
+		next = secret * 2048
+		secret = Mix(next, secret)
+		secret = Prune(secret)
+		allSecrets = append(allSecrets, secret)
+	}
+
+	return allSecrets
+}
+
 func Mix(val int, current int) int {
 	return val ^ current
 }
@@ -46,4 +67,133 @@ func Mix(val int, current int) int {
 func Prune(val int) int {
 	// note 16777216 is 2^23
 	return val % 16777216
+}
+
+type Price struct {
+	Actual int
+	Diff   int
+}
+
+func GetPriceAndDiff(secrets []int) []Price {
+	prices := []Price{}
+	for i, secret := range secrets {
+		diff := 0
+		price := secret % 10
+		if i != 0 {
+			diff = price - prices[i-1].Actual
+		}
+		prices = append(prices, Price{Actual: price, Diff: diff})
+	}
+
+	return prices
+}
+
+func FindBestPatternSum(allPrices [][]Price) int {
+	workerCtx, workerCancel := context.WithCancel(context.Background())
+	priceChan := make(chan []int, 100)
+	sumChan := make(chan int, 100)
+	workerwg := sync.WaitGroup{}
+	workerwg.Add(12)
+
+	for range 12 {
+		go func() {
+			defer workerwg.Done()
+			done := false
+			for {
+				select {
+				case <-workerCtx.Done():
+					done = true
+				case pattern := <-priceChan:
+					sum := SumAllPrices(pattern, allPrices)
+					// if sum > 20 {
+					// 	fmt.Fprintf(os.Stderr, "Found Good Sum with pattern: %d = (%d, %d, %d, %d)\n", sum, pattern[0], pattern[1], pattern[2], pattern[3])
+					// }
+					sumChan <- sum
+				}
+				if done {
+					break
+				}
+			}
+		}()
+	}
+
+	endCtx, endCancel := context.WithCancel(context.Background())
+	endwg := sync.WaitGroup{}
+	endwg.Add(1)
+	best := 0
+	go func() {
+		defer endwg.Done()
+		done := false
+		for {
+			select {
+			case <-endCtx.Done():
+				done = true
+			case test := <-sumChan:
+				if test > best {
+					best = test
+				}
+			}
+			if done {
+				break
+			}
+		}
+	}()
+
+	// generate work
+	for i := range 19 {
+		a := i - 9
+		for j := range 19 {
+			b := j - 9
+			for k := range 19 {
+				c := k - 9
+				for l := range 19 {
+					d := l - 9
+					priceChan <- []int{a, b, c, d}
+				}
+			}
+		}
+	}
+
+	fmt.Fprintf(os.Stderr, "Finished Generating Work\n")
+	// wait for workers to finish all processing
+	for len(priceChan) > 0 {
+	}
+	workerCancel()
+	workerwg.Wait()
+
+	fmt.Fprintf(os.Stderr, "Worker Queue Empty\n")
+	// wait for reducer to finish all processing
+	for len(sumChan) > 0 {
+	}
+	endCancel()
+	endwg.Wait()
+
+	fmt.Fprintf(os.Stderr, "Reducer Queue Empty\n")
+	// found the best
+	return best
+}
+
+func SumAllPrices(pattern []int, allPrices [][]Price) int {
+	sum := 0
+
+	for _, merchant := range allPrices {
+		for i := range len(merchant) - 4 {
+			if merchant[i].Diff != pattern[0] {
+				continue
+			}
+			if merchant[i+1].Diff != pattern[1] {
+				continue
+			}
+			if merchant[i+2].Diff != pattern[2] {
+				continue
+			}
+			if merchant[i+3].Diff != pattern[3] {
+				continue
+			}
+			sum += merchant[i+3].Actual
+			break
+		}
+	}
+
+	return sum
 }
--- 22/main_test.go
+++ 22/main_test.go
@@ -19,7 +19,7 @@ func TestSimplePart1(t *testing.T) {
 	assert.Equal(t, sum, 37327623)
 }
 
-func TestFirstSeedIterations(t *testing.T) {
+func TestFirstSeedIterationsPart1(t *testing.T) {
 	seeds := []int{123, 15887950, 16495136, 527345, 704524, 1553684, 12683156, 11100544, 12249484, 7753432, 5908254}
 	for i, seed := range seeds {
 		if i == len(seeds)-1 {
@@ -63,4 +63,75 @@ func TestFullPart1(t *testing.T) {
 	}
 
 	assert.Equal(t, sum, 18317943467)
+}
+
+func TestFirstSeedIterationsPart2(t *testing.T) {
+	seeds := []int{123, 15887950, 16495136, 527345, 704524, 1553684, 12683156, 11100544, 12249484, 7753432}
+	for i, seed := range seeds {
+		if i == len(seeds)-1 {
+			continue
+		}
+		result := GetNumForSeedIterations(seed, 1)
+		assert.Equal(t, result, seeds[i+1])
+	}
+
+	prices := GetPriceAndDiff(seeds)
+	expectedPrices := []Price{{Actual: 3, Diff: 0}, {Actual: 0, Diff: -3}, {Actual: 6, Diff: 6}, {Actual: 5, Diff: -1}, {Actual: 4, Diff: -1}, {Actual: 4, Diff: 0}, {Actual: 6, Diff: 2}, {Actual: 4, Diff: -2}, {Actual: 4, Diff: 0}, {Actual: 2, Diff: -2}}
+	assert.Equal(t, len(prices), len(expectedPrices))
+	for i, price := range prices {
+		assert.Equal(t, price.Actual, expectedPrices[i].Actual)
+		assert.Equal(t, price.Diff, expectedPrices[i].Diff)
+	}
+}
+
+func TestGetAllSeedIterations(t *testing.T) {
+	expected := []int{123, 15887950, 16495136, 527345, 704524, 1553684, 12683156, 11100544, 12249484, 7753432}
+	seeds := GetAllSeedIterations(123, 9)
+	assert.Equal(t, len(seeds), len(expected))
+	for i := range seeds {
+		assert.Equal(t, seeds[i], expected[i])
+	}
+}
+
+func TestGenerator(t *testing.T) {
+	expected := []int{-9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+	actual := []int{}
+	for i := range 19 {
+		actual = append(actual, i-9)
+	}
+
+	assert.Equal(t, len(actual), len(expected))
+	for i := range actual {
+		assert.Equal(t, actual[i], expected[i])
+	}
+}
+
+func TestSimplePart2(t *testing.T) {
+	content := lib.ReadInput("test2.txt")
+	assert.NotEqual(t, content, "")
+	seeds := GetSeeds(content)
+	prices := [][]Price{}
+	for _, seed := range seeds {
+		secrets := GetAllSeedIterations(seed, 2000)
+		merchantPrices := GetPriceAndDiff(secrets)
+		prices = append(prices, merchantPrices)
+	}
+	sum := FindBestPatternSum(prices)
+
+	assert.Equal(t, sum, 23)
+}
+
+func TestFullPart2(t *testing.T) {
+	content := lib.ReadInput("input.txt")
+	assert.NotEqual(t, content, "")
+	seeds := GetSeeds(content)
+	prices := [][]Price{}
+	for _, seed := range seeds {
+		secrets := GetAllSeedIterations(seed, 2000)
+		merchantPrices := GetPriceAndDiff(secrets)
+		prices = append(prices, merchantPrices)
+	}
+	sum := FindBestPatternSum(prices)
+
+	assert.Equal(t, sum, 2018)
 }
